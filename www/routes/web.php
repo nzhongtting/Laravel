63a1b5cc379cfd892f225b1f66569bd3c49629a9
Thomas Hong <hongtting@hotmail.com>
2019-04-01 09:18:37 +0900

    URL 통해 변수 넘기기

--- www/routes/web.php
+++ www/routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Route::get('/hello', function () {
+    // return view('welcome');
+    return "Hello World";
+});
+*/
+
+Route::get('/about',function() {
+    return view('pages.about');
+} );
+
+Route::get('/user/{id}',function($id) {
+    return 'This is user : '.$id;
+} );
+
